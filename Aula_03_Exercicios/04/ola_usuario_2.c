f5686b804f2b29132f48c05870598507e845a556
CFerrer <camila.c.ferrer@gmail.com>
2018-03-16 15:33:46 -0300

    Solution Exercise 04

---
+++ Aula_03_Exercicios/04/ola_usuario_2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if(argc<2){
+		printf(" ----------------------------\n");
+		printf("|   Não foi inserido nome    |\n");
+		printf("| na chamada deste programa. |\n");
+		printf("| Por exemplo:               |\n");
+		printf("|    $ ./ola_usuario_2 Eu    |\n");
+		printf("|         Ola Eu             |\n");
+		printf("|    $                       |\n");
+		printf(" ----------------------------\n");
+		return -1;
+	}
+	
+	printf("Ola");
+	for(int i = 1; i<argc; i++){
+		printf(" %s", argv[i]);
+	}
+	printf("\n");
+
+	return -1;
+}
